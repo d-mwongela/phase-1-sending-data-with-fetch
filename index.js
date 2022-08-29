89da69cc91897f0d5cd44969376bfee0e30c0eb4
dennis00095 <dennis.mwongela@student.moringaschool.com>
2022-08-29 23:11:56 +0300

    Completedassignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData (name,email) {
+    const configObj = {
+        method: "POST",
+        headers : {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({name, email})
+    }
+    return fetch('http://localhost:3000/users', configObj)
+    .then(function (resp){
+        return resp.json()
+    })
+    .then(function (data){
+        return document.body.append(data.id)
+    })
+    .catch(function (error){
+        return document.body.append("Unauthorized Access")
+        
+    })
+}
+submitData()
